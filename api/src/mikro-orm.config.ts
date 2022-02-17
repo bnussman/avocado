3bdc1f33f1479d98d168626d654a142bd8840eee
Banks Nussman <banks@nussman.us>
2022-02-17 14:03:50 -0500

    Connect to db on localhost

--- api/src/mikro-orm.config.ts
+++ api/src/mikro-orm.config.ts
@@ -6,7 +6,8 @@ export default {
   user: 'root',
   password: 'avocado',
   type: 'mysql',
-  clientUrl: `mysql://root@db:3306/avocado`,
+  // clientUrl: `mysql://root@db:3306/avocado`,
+  clientUrl: `mysql://root@localhost:3306/avocado`,
   loadStrategy: LoadStrategy.JOINED,
   debug: true,
 } as unknown as Configuration<IDatabaseDriver<Connection>>
